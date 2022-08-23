9767fe6c4027c45aef5fc69dcf6bdb981ee3bd9f
Antont25 <toxafootbol1994@gmail.com>
2022-08-23 10:54:35 +0300

    refactor CheckEmail.tsx

--- src/app/store.ts
+++ src/app/store.ts
@@ -17,9 +17,8 @@ declare global {
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const store = legacy_createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
-
+//type
 export type AppActionType = ActionPasswordRecoveryType
-
 export type RootState = ReturnType<typeof rootReducer>
 export type AppDispatch = ThunkDispatch<RootState, unknown, AppActionType>
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, AppActionType>
