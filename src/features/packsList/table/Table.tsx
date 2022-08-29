224c1c4b8bff0acea98b293c35382d4ecd7e5707
Antont25 <toxafootbol1994@gmail.com>
2022-08-29 21:58:34 +0300

    add  Cards.tsx

--- src/features/packsList/table/Table.tsx
+++ src/features/packsList/table/Table.tsx
@@ -1,24 +1,23 @@
 import React, {useEffect} from 'react';
-import {TableContainer} from "@mui/material";
+import {TableContainer} from '@mui/material';
 import Table from '@mui/material/Table';
 import Paper from '@mui/material/Paper';
-import {useAppSelector} from "../../../common/hooks/useAppSelector";
-import {useAppDispatch} from "../../../common/hooks/useAppDispatch";
-import {getPacksTC} from "../packs-reducer";
-import {CustomTableBody} from "./tableBody/CustomTableBody";
-import {CustomTableHead} from "./TableHead/CustomTableHead";
-import {Loading} from "../../../common/components/Loading/Loading";
+import {useAppSelector} from '../../../common/hooks/useAppSelector';
+import {useAppDispatch} from '../../../common/hooks/useAppDispatch';
+import {getPacksTC} from '../packs-reducer';
+import {CustomTableBody} from './tableBody/CustomTableBody';
+import {CustomTableHead} from './TableHead/CustomTableHead';
+import {Loading} from '../../../common/components/Loading/Loading';
 
 export type HeadCellType = {
-    sortPart: "name"
-        | "updated"
-        | "cardsCount"
-        | "user_name"
-        | "actions"
+    sortPart: 'name'
+        | 'updated'
+        | 'cardsCount'
+        | 'user_name'
+        | 'actions'
     sortable: boolean
     title: string
 }
-
 
 export const CardsTable = () => {
 
@@ -38,28 +37,28 @@ export const CardsTable = () => {
 
         const headCells: Array<HeadCellType> = [
             {
-                sortPart: "name",
-                title: "Name",
+                sortPart: 'name',
+                title: 'Name',
                 sortable: true,
             },
             {
-                sortPart: "cardsCount",
-                title: "Cards",
+                sortPart: 'cardsCount',
+                title: 'Cards',
                 sortable: true,
             },
             {
-                sortPart: "updated",
-                title: "Last Updated",
+                sortPart: 'updated',
+                title: 'Last Updated',
                 sortable: true,
             },
             {
-                sortPart: "user_name",
-                title: "Created by",
+                sortPart: 'user_name',
+                title: 'Created by',
                 sortable: true,
             },
             {
-                sortPart: "actions",
-                title: "Actions",
+                sortPart: 'actions',
+                title: 'Actions',
                 sortable: false,
             },
 
